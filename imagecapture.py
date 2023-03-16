243c97138fbb3b75bd5d104e7fffb12c68d9664c
Andre Ramos <ralphandrer04@gmail.com>
2023-03-16 15:20:00 -0700

    Created image capture file

---
+++ imagecapture.py
@@ -0,0 +1,24 @@
+
+# TODO - Import Libraries 
+
+
+
+
+
+# TODO - Image Labels
+
+
+
+
+# TODO - Folder Setup / Training and Testing Folders
+
+
+
+
+
+# TODO - Capture Images
+
+
+
+
+# TODO - Labeling Images 
