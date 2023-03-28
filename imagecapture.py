2191913cfc5db302ec7eb3f18527dc26ed7c1dec
Yujing <yujingwang1023@gmail.com>
2023-03-27 18:15:08 -0700

    update imagecapture.py with folder setup

--- imagecapture.py
+++ imagecapture.py
@@ -15,6 +15,19 @@ import os
 
 # TODO - Folder Setup / Training and Testing Folders
 
+print(cv2.__version__)
+
+IMAGE_PATH = os.path.join( 'Tensorflow', 'workspace', 'images', 'collectedimages')
+if not os.path. exists (IMAGE_PATH):
+        os.makedirs (IMAGE_PATH)
+labels = ['anger', 'fear', 'disgust', 'happy' , 'sad' , 'neutral' , 'suprised']
+num_img = 1
+for label in labels:
+    path = os.path. join (IMAGE_PATH, label)
+    if not os.path.exists (path): 
+            os.makedirs (path)
+
+
 
 
 
