25f64f2ff19901fc9b9988946f8fbf4ede46b63c
Andre Ramos <ralphandrer04@gmail.com>
2023-04-13 16:30:03 -0700

    Added names for models

--- detect.py
+++ detect.py
@@ -1,2 +1,8 @@
 import os 
 
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
+PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
+PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
+TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
+LABEL_MAP_NAME = 'label_map.pbtxt'
+
