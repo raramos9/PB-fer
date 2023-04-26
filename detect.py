40201294034cd987bbf6f40bac6d3f3d537a3b61
KyleL1015 <longkyle2022@gmail.com>
2023-04-25 18:48:29 -0700

    added wget download

--- detect.py
+++ detect.py
@@ -1,10 +1,14 @@
-import os 
+import os
+
 
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
 PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
 TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
 LABEL_MAP_NAME = 'label_map.pbtxt'
+
+if os.name == 'nt':
+    wget.download(PRETRAINED_MODEL_URL)
 
 paths = {
     'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
